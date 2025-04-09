19f8550de07846b11a173ee3fa2cf29041dfb22a
Jan Neuenfeld <85286163+JanNeuenfeld@users.noreply.github.com>
2025-04-09 16:41:36 +0200

    improve macro

--- src/error.rs
+++ src/error.rs
@@ -1,13 +1,10 @@
 use warp::reject::{Reject, Rejection};
 
 macro_rules! new_rejections {
-    ($i:ident) => {
+    ($i0:ident $(, $i:ident)*) => {
         #[derive(Debug)]
-        pub struct $i;
-        impl Reject for $i {}
-    };
-    ($i0:ident $(, $i:ident)+) => {
-        new_rejections!($i0);
+        pub struct $i0;
+        impl Reject for $i0 {}
         $(new_rejections!($i);)*
     };
 }
