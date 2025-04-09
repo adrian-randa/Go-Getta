0e95d24f06fb6e82419b5519125e13c5c0cff8ec
Jan Neuenfeld <85286163+JanNeuenfeld@users.noreply.github.com>
2025-04-09 14:59:04 +0200

    improve macro

--- src/error.rs
+++ src/error.rs
@@ -1,18 +1,24 @@
 use warp::reject::{Reject, Rejection};
 
-macro_rules! new_rejection {
+macro_rules! new_rejections {
     ($i:ident) => {
         #[derive(Debug)]
         pub struct $i;
         impl Reject for $i {}
     };
+    ($i0:ident $(, $i:ident)+) => {
+        new_rejections!($i0);
+        $(new_rejections!($i);)*
+    };
 }
 
-new_rejection!(InvalidKeyError);
-new_rejection!(UserAlreadyExistsError);
-new_rejection!(InvalidPasswordError);
-new_rejection!(InternalServerError);
-new_rejection!(InvalidSessionError);
-new_rejection!(InvalidQueryError);
-new_rejection!(InvalidFileError);
-new_rejection!(UserDoesNotExistError);
+new_rejections!(
+    InvalidKeyError,
+    UserAlreadyExistsError,
+    InvalidPasswordError,
+    InternalServerError,
+    InvalidSessionError,
+    InvalidQueryError,
+    InvalidFileError,
+    UserDoesNotExistError
+);
