c7b5145b301b367a0a785d7b1676bf7690ec0b77
Adrian <adrian.juengling@gmail.com>
2025-06-07 12:17:02 +0200

    fix error not being returned to admin in run_sql

--- src/admin.rs
+++ src/admin.rs
@@ -1,5 +1,6 @@
 use diesel::{sql_query, RunQueryDsl};
 use serde::Deserialize;
+use warp::reject::Reject;
 
 use std::ops::DerefMut;
 
@@ -11,9 +12,16 @@ pub struct RunSqlData {
     sql: String,
 }
 
+#[derive(Debug)]
+struct RunSqlError {
+    message: String,
+}
+
+impl Reject for RunSqlError {}
+
 pub async fn run_sql(connection: DBConnection, data: RunSqlData) -> Result<impl warp::Reply, warp::Rejection> {
 
-    sql_query(data.sql).execute(connection.lock().await.deref_mut()).map_err(|e| warp::reply::with_status(format!("Error: {:?}", e), warp::http::StatusCode::BAD_REQUEST));
+    sql_query(data.sql).execute(connection.lock().await.deref_mut()).map_err(|e| RunSqlError{ message: e.to_string() })?;
 
     Ok(warp::reply::with_status("Executed!", warp::http::StatusCode::ACCEPTED))
 }
